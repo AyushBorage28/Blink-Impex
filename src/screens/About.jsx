b92731ef434f824fc2fa538b31b220ada122046d
Ayush Borage <ayushborage28@gmail.com>
2023-06-04 00:10:09 +0530

    templateScreen

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import backgroundImage from "../assets/about-bg.jpg";
+import mobileBackgroundImage from "../assets/about-bg-mobile.jpg";
+import BackgroundImage from "../components/BackgroundImage";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <BackgroundImage
+        backgroundImage={backgroundImage}
+        mobileBackgroundImage={mobileBackgroundImage}
+        backgroundPosition="center center"
+        mobileBackgroundPosition="center center"
+      ></BackgroundImage>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/screens/Products.jsx
+++ src/screens/Products.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import backgroundImage from "../assets/product-bg.jpg";
+import mobileBackgroundImage from "../assets/about-bg-mobile.jpg";
+import BackgroundImage from "../components/BackgroundImage";
+
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <>
+    <BackgroundImage
+      backgroundImage={backgroundImage}
+      mobileBackgroundImage={mobileBackgroundImage}
+      backgroundPosition="right center"
+      mobileBackgroundPosition="center center"
+    ></BackgroundImage>
+  </>
   )
 }
 
