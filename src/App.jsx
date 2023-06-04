bf0898e327ba60068c9436427a32743270dcbc92
Ayush Borage <ayushborage28@gmail.com>
2023-06-05 03:14:23 +0530

    fix refresh bug-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import About from "./screens/About.jsx";
@@ -12,28 +12,34 @@ import ScrollToTop from "./components/ScrollToTop.jsx";
 import LogoLoader from "./components/LogoLoader.jsx";
 
 const App = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  React.useEffect(() => {
+  useEffect(() => {
     // Simulating data loading delay
     setTimeout(() => {
       setIsLoading(false);
-    }, 4000);
+    }, 3000);
   }, []);
 
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<HomeScreen />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/technicalArea" element={<TechnicalArea />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        <Route path="/exports" element={<Exports />} />
-        <Route path="/contactUs" element={<ContactScreen />} />
-      </Routes>
-      <Footer />
+      {isLoading ? (
+        <LogoLoader />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/technicalArea" element={<TechnicalArea />} />
+            <Route path="/catalogue" element={<Catalogue />} />
+            <Route path="/exports" element={<Exports />} />
+            <Route path="/contactUs" element={<ContactScreen />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </BrowserRouter>
   );
 };
